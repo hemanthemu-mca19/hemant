3d11060f413e1214bf2af81bbc2481cf0d78f251
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-04-04 18:02:02 +0530

    Update longestValidParentheses.cpp

--- Leetcode/Leetcode Daily/April/longestValidParentheses.cpp
+++ Leetcode/Leetcode Daily/April/longestValidParentheses.cpp
@@ -18,3 +18,28 @@ public:
         return res;
     }
 };
+
+
+//2nd Approach
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        
+        int n = s.length();
+        int res = 0;
+        for(int i=0; i<n; i++) {
+            if( s[i] == '(' )    
+                st.push(i);
+            else {
+                st.pop();
+                
+                if( st.empty() ) st.push(i);
+                else  res =  max(res,i - st.top() );
+                }
+            }
+        return res;
+    }
+};
