8d143da05e5a8f76a549b3c96b34464976f0628e
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-04-29 22:37:16 +0530

    Create matrixTranspose.cpp
    Problem Link: https://binarysearch.com/problems/Transpose-of-a-Matrix

---
+++ BinarySearch/matrixTranspose.cpp
@@ -0,0 +1,13 @@
+vector<vector<int>> solve(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    for(int i=0; i<n; i++) {
+        for(int j=i; j<n; j++) {
+            if(i!=j){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    return matrix;
+}
