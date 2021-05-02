cc7868ad17a1d180e71a8b64257e66fd9cf2fe79
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-02 21:39:43 +0530

    Update largestBy2Times.cpp

--- BinarySearch/largestBy2Times.cpp
+++ BinarySearch/largestBy2Times.cpp
@@ -5,3 +5,19 @@ bool solve(vector<int>& nums) {
     if(nums[n-1]>2*nums[n-2]) return true;
     else return false;  
 }
+//O(n) Approach
+bool solve(vector<int>& nums) {
+    int first = nums[0], second = INT_MIN;
+    int n = nums.size();
+    for(int i=1; i<n; i++){
+        if(nums[i]>first){
+            second = first;
+            first =  nums[i];    
+        }
+        else if(nums[i]>second){
+            second = nums[i];
+        }
+    }
+    if (first>2*second) return true;
+    else return false;
+}
