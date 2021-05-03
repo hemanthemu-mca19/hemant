31a60c45097c5bebea821da9984609483d6085cb
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-03 15:52:36 +0530

    Update GuessRoot.cpp

--- BinarySearch/GuessRoot.cpp
+++ BinarySearch/GuessRoot.cpp
@@ -5,3 +5,19 @@ int solve(int n) {
    }
    return 0; 
 }
+
+//2nd Approach
+/**
+x^2 = 9
+log x^2 = log 9
+2 log x = log 9
+log x = log 9 /2
+x = exponential(log 9 / 2)
+**/
+
+//Code
+
+int solve(int n) {
+   int x = exp(log(n)/2);
+   return x;
+}
