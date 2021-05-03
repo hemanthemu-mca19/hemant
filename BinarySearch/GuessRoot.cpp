b7f0ada9e8db1da03fd29a2a7e6ed377b7197121
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-03 16:06:03 +0530

    Update GuessRoot.cpp

--- BinarySearch/GuessRoot.cpp
+++ BinarySearch/GuessRoot.cpp
@@ -6,7 +6,20 @@ int solve(int n) {
    return 0; 
 }
 
-//2nd Approach
+//2nd Approach (Binary Search)
+
+int solve(int n) {
+   long int l=0, h=n, mid;
+   while(l<=h){
+       mid = (l+h)/2;
+       if(mid*mid == n) return mid;
+       else if(mid*mid > n) h=mid-1;
+       else l=mid+1;
+   }
+   return h;
+}
+
+//3rd Approach
 /**
 x^2 = 9
 log x^2 = log 9
