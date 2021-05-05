c903a695717153884339e59764c32b62600888cd
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-06 03:19:59 +0530

    Update evenFrequency.cpp

--- BinarySearch/evenFrequency.cpp
+++ BinarySearch/evenFrequency.cpp
@@ -9,3 +9,13 @@ bool solve(vector<int>& nums) {
     }
     return true;
 }
+
+//Bit MAnipulation Approach
+bool solve(vector<int>& nums) {
+    int a = 0, b = 0;
+    for (auto x : nums) {
+        a ^= x;
+        b ^= x + 1;
+    }
+    return !a && !b;
+}
