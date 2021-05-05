3cce9ed3151e90458c6dc915ba8b69102b91f74d
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-06 03:17:13 +0530

    Create evenFrequency.cpp
    Problem Link: https://binarysearch.com/problems/Even-Frequency

---
+++ BinarySearch/evenFrequency.cpp
@@ -0,0 +1,11 @@
+bool solve(vector<int>& nums) {
+    unordered_map<int, int> mp;
+    if(nums.size()%2 !=0) return false;
+    for(int i=0; i<nums.size(); i++){
+        mp[nums[i]]++;
+    }
+    for(auto x: mp) {
+        if(x.second %2 !=0) return false;
+    }
+    return true;
+}
