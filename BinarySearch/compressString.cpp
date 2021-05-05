1fd2da9a38c95266a8e8889329a97fdcb2bc812b
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-06 00:28:22 +0530

    Update compressString.cpp

--- BinarySearch/compressString.cpp
+++ BinarySearch/compressString.cpp
@@ -7,3 +7,12 @@ string solve(string s) {
     }
     return s1;
 }
+
+//O(n) Approach and O(1) Space
+string solve(string s) {
+    int j=1;
+    for(int i=1; i<s.size(); i++) {
+        if(s[i]!=s[i-1]) s[j++] = s[i];
+    }
+    return s.substr(0,j);
+}
