5cf6ee647990e7557d4aa7dd04d05e05b84cdc26
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-05 15:38:17 +0530

    Optimized Space to O(1)

--- BinarySearch/MinBracketAdd.cpp
+++ BinarySearch/MinBracketAdd.cpp
@@ -1,3 +1,6 @@
+//O(n) space 
+//O(n) time complexity
+
 int solve(string s) {
     stack<char> st;
     for(int i=0; i<s.size(); i++){
@@ -9,4 +12,16 @@ int solve(string s) {
         else st.push(s[i]);
     }
     return st.size();
+}
+
+
+// Space Optimised Code O(1) space
+int solve(string s) {
+    int left =0, stack=0;
+    for(auto c: s){
+        if(!stack and c==')') left++;
+        else if(c=='(') stack++;
+        else stack--;
+    }
+    return stack+left;
 }
