75442cf4710f43f98c0b0d69682c0cf22c9b01d5
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-13 14:49:08 +0530

    Update Pascal'sTriangle.cpp

--- BinarySearch/Pascal'sTriangle.cpp
+++ BinarySearch/Pascal'sTriangle.cpp
@@ -1,5 +1,5 @@
 /** https://binarysearch.com/problems/Pascal's-Triangle **/
-
+//Recursive Approach
 vector<int> getRow(int n){
     vector<int> curr;
     curr.push_back(1);
@@ -17,4 +17,16 @@ vector<int> getRow(int n){
 vector<int> solve(int n) {
     vector<int> p = getRow(n);
     return p;
+}
+
+//Iterative Approach
+vector<int> solve(int n) {
+    vector<int> temp;
+    temp.push_back(1);
+    while (n--) {
+        cout<<temp.size()<<" ";
+        for (int i = temp.size() - 1; i > 0; i--) temp[i] += temp[i - 1];
+        temp.push_back(1);
+    }
+    return temp;
 }
