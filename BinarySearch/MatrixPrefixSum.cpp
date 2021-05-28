3f68dd432025650345900950399e278f43b01311
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-05-29 00:41:06 +0530

    Update MatrixPrefixSum.cpp

--- BinarySearch/MatrixPrefixSum.cpp
+++ BinarySearch/MatrixPrefixSum.cpp
@@ -46,3 +46,22 @@ vector<vector<int>> solve(vector<vector<int>>& matrix) {
     }
     return newMatrix;
     }
+
+// Space Optimization
+
+vector<vector<int>> solve(vector<vector<int>>& matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            if (j != 0) {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+        for (int j = 0; j < matrix[0].size(); j++) {
+            if (i != 0) {
+                matrix[i][j] += matrix[i - 1][j];
+            }
+        }
+    }
+
+    return matrix;
+}
