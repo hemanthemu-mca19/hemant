2916589267ab79320f7a6c1c4623831759baaded
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-06-01 16:32:38 +0530

    Create MaxAreaIsland.cpp

---
+++ Leetcode/Leetcode Daily/June/MaxAreaIsland.cpp
@@ -0,0 +1,58 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/603/week-1-june-1st-june-7th/3764/
+
+/** 
+ Max Area of Island
+
+Solution
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+**/
+
+// CODE
+
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int row, int col) {
+        if(row < 0 || col < 0 || row >= grid.size() || col >= grid[row].size() || grid[row][col] == 0) return 0;
+        
+        grid[row][col] = 0;
+        
+        return (1 + dfs(grid, row+1, col) + dfs(grid, row, col+1) + dfs(grid, row-1, col) + dfs(grid, row, col-1));
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int max_area = 0;
+        
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[i].size(); j++) {
+                max_area = max(max_area, dfs(grid, i, j));
+            }
+        }
+        return max_area;
+    }
+    
+};
