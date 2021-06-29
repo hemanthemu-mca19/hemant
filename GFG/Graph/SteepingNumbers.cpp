5dae2befcc278093b6527e19c5b3be992f9eb0a2
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-06-29 21:24:02 +0530

    Create  SteepingNumbers.cpp

---
+++ GFG/Graph/SteepingNumbers.cpp
@@ -0,0 +1,55 @@
+// https://practice.geeksforgeeks.org/problems/stepping-numberswrong-output1813/1#
+
+/**
+Stepping Numbers 
+Medium Accuracy: 52.35% Submissions: 693 Points: 4
+A number is called a stepping number if all adjacent digits have an absolute difference of 1, e.g. '321' is a Stepping Number while 421 is not. Given two integers n and m, find the count of all the stepping numbers in the range [n, m].
+
+Examples:
+
+Input1 : n = 0, m = 21
+Output1 : 13
+Stepping no's are 0 1 2 3 4 5 6 7 8 
+9 10 12 21
+
+Input2 : n = 10, m = 15
+Output2 : 2
+Stepping no's are 10, 12
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function steppingNumbers() which takes the integer N and integer M as input parameters and returns the number of stepping numbers in the range between n and m.
+
+Expected Time Complexity: O(log(M))
+Expected Auxiliary Space: O(SN) where SN is the number of stepping numbers in the range
+
+
+Constraints:
+0 ≤ N, M ≤ 106
+**/
+
+// CODE O(logM)TC O(SteppingNumbers)SC
+
+Stepping Numbers 
+Medium Accuracy: 52.35% Submissions: 693 Points: 4
+A number is called a stepping number if all adjacent digits have an absolute difference of 1, e.g. '321' is a Stepping Number while 421 is not. Given two integers n and m, find the count of all the stepping numbers in the range [n, m].
+
+Examples:
+
+Input1 : n = 0, m = 21
+Output1 : 13
+Stepping no's are 0 1 2 3 4 5 6 7 8 
+9 10 12 21
+
+Input2 : n = 10, m = 15
+Output2 : 2
+Stepping no's are 10, 12
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function steppingNumbers() which takes the integer N and integer M as input parameters and returns the number of stepping numbers in the range between n and m.
+
+Expected Time Complexity: O(log(M))
+Expected Auxiliary Space: O(SN) where SN is the number of stepping numbers in the range
+
+
+Constraints:
+0 ≤ N, M ≤ 106
