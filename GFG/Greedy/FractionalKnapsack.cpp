fe724833a0639b3ccef312795009bbf54c046c28
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-07-01 11:17:22 +0530

    Create FractionalKnapsack.cpp

---
+++ GFG/Greedy/FractionalKnapsack.cpp
@@ -0,0 +1,122 @@
+//https://practice.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1#
+
+/**
+Fractional Knapsack 
+Medium Accuracy: 45.14% Submissions: 27903 Points: 4
+Given weights and values of N items, we need to put these items in a knapsack of capacity W to get the maximum total value in the knapsack.
+Note: Unlike 0/1 knapsack, you are allowed to break the item. 
+
+ 
+
+Example 1:
+
+Input:
+N = 3, W = 50
+values[] = {60,100,120}
+weight[] = {10,20,30}
+Output:
+240.00
+Explanation:Total maximum value of item
+we can have is 240.00 from the given
+capacity of sack. 
+Example 2:
+
+Input:
+N = 2, W = 50
+values[] = {60,100}
+weight[] = {10,20}
+Output:
+160.00
+Explanation:
+Total maximum value of item
+we can have is 160.00 from the given
+capacity of sack.
+ 
+
+Your Task :
+Complete the function fractionalKnapsack() that receives maximum capacity , array of structure/class and size n and returns a double value representing the maximum value in knapsack.
+Note: The details of structure/class is defined in the comments above the given function.
+
+
+Expected Time Complexity : O(NlogN)
+Expected Auxilliary Space: O(1)
+
+
+Constraints:
+1 <= N <= 105
+1 <= W <= 105
+**/
+
+//CODE
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+ // } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+static bool comp(Item &a, Item &b){
+    return (double)a.value/a.weight > (double)b.value/b.weight;
+}
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        if(W == 0 || n == 0) return 0;
+        sort(arr, arr+n, comp);
+        double total_value = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i].weight <= W){
+                W -= arr[i].weight;
+                total_value += arr[i].value;
+            }
+            else {
+                total_value += (((double)arr[i].value/ arr[i].weight) * W);
+                break;
+            }
+        }
+        return total_value;
+    }
+        
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(2)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
