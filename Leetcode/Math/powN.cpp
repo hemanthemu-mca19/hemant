97e3e2f93280bd2b4d9b14c485f7fe4fe1d6edef
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-07-04 18:39:30 +0530

    Create powN.cpp

---
+++ Leetcode/Math/powN.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/powx-n/
+
+/**
+Pow(x, n)
+Medium
+
+2622
+
+3988
+
+Add to List
+
+Share
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+-104 <= xn <= 104
+**/
+
+// CODE
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        long nn = abs(n);
+        double ans = 1;
+        
+        while(nn>1) {
+            if(nn%2 == 0){
+                x *= x;
+                nn /= 2;
+            }
+            else {
+                ans *= x;
+                nn -=1;
+            }
+            
+        }
+        ans *=x;
+        if(n < 0) ans = 1/ans;
+        return ans;
+    }
+};
