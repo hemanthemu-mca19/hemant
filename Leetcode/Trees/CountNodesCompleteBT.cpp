9346c022304eeafb504ef4974a2e4667cf803dc4
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-07-23 12:01:06 +0530

    Count Complete Tree Nodes

---
+++ Leetcode/Trees/CountNodesCompleteBT.cpp
@@ -0,0 +1,78 @@
+// https://leetcode.com/problems/count-complete-tree-nodes/
+
+/**
+Count Complete Tree Nodes
+Medium
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+**/
+
+// code
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        int ans = 0;
+        if(!root) return ans;
+        queue<TreeNode* > q;
+        q.push(root);
+        bool flag = false;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                TreeNode* curr = q.front();
+                q.pop();
+                ans++;
+                if(!curr->left) {
+                    flag = true;
+                    break;
+                }
+                else q.push(curr->left);
+                if(!curr->right) {
+                    flag = true;
+                    break;
+                }
+                else q.push(curr->right); 
+            }
+            if(flag == true) break;
+        }
+       return ans+q.size(); 
+    }
+};
