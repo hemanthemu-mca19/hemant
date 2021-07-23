64609320377a88200bf571bd9c087779c6ef8aec
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-07-23 12:37:16 +0530

    Update CountNodesCompleteBT.cpp

--- Leetcode/Trees/CountNodesCompleteBT.cpp
+++ Leetcode/Trees/CountNodesCompleteBT.cpp
@@ -46,6 +46,8 @@ The tree is guaranteed to be complete.
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// code
 class Solution {
 public:
     int countNodes(TreeNode* root) {
@@ -74,5 +76,40 @@ public:
             if(flag == true) break;
         }
        return ans+q.size(); 
+    }
+};
+
+// space optimized
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        int left_levels = 1;
+        int right_levels = 1;
+        TreeNode *l = root->left, *r = root->right;
+        while(l){
+            left_levels++;
+            l = l->left;
+        }
+        while(r){
+            right_levels++;
+            r = r->right;
+        }
+        if(left_levels == right_levels) {
+            return pow(2,left_levels)-1;
+        }
+        
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
